ca1af9cac2ef8749c1f1531cf7813c193b7d5304
olaniyi <arokoyueb11@gmail.com>
2024-10-17 20:05:38 +0100

    style update commit

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex py-3 flex-wrap justify-around'>
+    <div className='flex p-6 flex-wrap justify-between'>
           <h1 className='text-2xl font-semibold'>Todo-App</h1>
           <ul className='flex gap-10'>
               <ThemeSwitcher />
--- Components/Todo.jsx
+++ Components/Todo.jsx
@@ -4,7 +4,8 @@ const Todo = ({id, title, description, mongoId, complete, deleteTodo, completeTo
   return (
        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {id+1}
+              {id + 1}
+              n
                 </th>
                 <td className={`px-6 py-4 ${complete ? 'line-through' : ''}`}>
                     {title}
